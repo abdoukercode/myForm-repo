fa2ec92543a7d8bf66f82cee6e1453f3cb7ecec0
abdoukercode <acamara0123@gmail.com>
2016-07-04 16:11:59 +0200

    second

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>The fucking very first angy training form</h1>'
+    template: '<h1>all fucking checkout</h1>'
 })
 export class AppComponent { }
