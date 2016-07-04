a7bd87cca7d8cb5fe23455946a052afdb216e9a5
abdoukercode <acamara0123@gmail.com>
2016-07-04 16:18:27 +0200

    changer h1 encore une fois

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>all fucking checkout</h1>'
+    template: '<h1>all 4 one fucking checkout</h1>'
 })
 export class AppComponent { }
